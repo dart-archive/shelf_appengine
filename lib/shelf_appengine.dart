3cdf8810f0fa99bfe44313f30b021e8004a6c731
Kevin Moore <github@j832.com>
2014-11-10 13:00:56 -0800

    code cleanup

--- lib/shelf_appengine.dart
+++ lib/shelf_appengine.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// Provides hosting of `Shelf` handlers via App Engine.
 library shelf_appengine;
 
 import 'dart:async';
@@ -12,12 +13,15 @@ import 'package:mime/mime.dart' as mime;
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
+/// Serves the provided [handler] using [ae.runAppEngine].
 Future serve(Handler handler, {Function onError}) {
   return ae.runAppEngine((io.HttpRequest request) {
     shelf_io.handleRequest(request, handler);
   }, onError: onError);
 }
 
+/// Serves files using [ae.context.assets].
+//TODO(kevmoo) better docs.
 final Handler assetHandler = _assetHandler;
 
 _assetHandler(Request request) {
@@ -27,13 +31,15 @@ _assetHandler(Request request) {
     Map headers;
     var contentType = mime.lookupMimeType(path);
     if (contentType != null) {
-      headers = <String, String>{io.HttpHeaders.CONTENT_TYPE: contentType};
+      headers = <String, String>{
+        io.HttpHeaders.CONTENT_TYPE: contentType
+      };
     }
 
     return new Response.ok(stream, headers: headers);
   }, onError: (err, stack) {
-    ae.context.services.logging.error('Error getting asset at path $path\n'
-        '$err\n$stack');
+    ae.context.services.logging.error(
+        'Error getting asset at path $path\n$err\n$stack');
     // TODO(kevmoo): handle only the specific case of an asset not found
     // https://github.com/dart-lang/appengine/issues/7
     if (err is ae.AssetError) {
